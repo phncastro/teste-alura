f7ea5d62db6afaa62e8a1146137e62698c518c78
Pablo Henrique <phncastro3@gmail.com>
2025-12-05 03:10:47 -0300

    versao 1 teste

---
+++ teste.py
@@ -0,0 +1,15 @@
+nome = input("Digite aqui seu nome: ")
+idade = int(input("Digite aqui sua idade: "))
+condicao = 1
+
+while condicao:
+    if idade >= 18:
+        print("Você é maior de idade!")
+    elif idade > 0 and idade < 18:
+        print("Você é menor de idade!")
+    elif idade > 99:
+        print("Idade inválida!")
+    else:
+        print("Digite uma idade válida!")
+    if isinstance(idade, int):
+        break
