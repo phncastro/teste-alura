b8117f88b2b3f731ab14f1eeb42f816fe9f82592
Pablo Henrique <phncastro3@gmail.com>
2025-12-05 03:17:45 -0300

    adicionar funcao saudar
    Introduced a greeting function to enhance user interaction.

--- teste.py
+++ teste.py
@@ -2,6 +2,13 @@ nome = input("Digite aqui seu nome: ")
 idade = int(input("Digite aqui sua idade: "))
 condicao = 1
 
+def saudar(saudacao, nome):
+    print(f"{saudacao} {nome}!")
+
+saudar("Bom dia", "Pablo")
+
+    
+
 while condicao:
     if idade >= 18:
         print("Você é maior de idade!")
@@ -13,3 +20,4 @@ while condicao:
         print("Digite uma idade válida!")
     if isinstance(idade, int):
         break
+        
